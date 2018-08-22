0b6db08ea6b19f7ec97f127c553f3835fed566e8
David <david-zimmermann@hotmail.de>
2018-08-22 17:31:58 +0200

    updated readme to show the graphics

--- images/create_images.R
+++ images/create_images.R
@@ -22,11 +22,12 @@ joins <- c(full_join = animate_full_join,
            inner_join = animate_inner_join,
            left_join = animate_left_join,
            right_join = animate_right_join,
-           semi_join = animate_right_join)
+           semi_join = animate_semi_join)
 
 a <- sapply(1:length(joins), function(i) {
   nam <- names(joins)[i]
   nam <- str_replace(nam, "_", "-")
+  cat(nam, "\n")
 
   width <- 7
   height <- 7
@@ -74,12 +75,14 @@ a <- sapply(1:length(sets), function(i) {
   nam <- names(sets)[i]
   nam <- str_replace(nam, "_", "-")
 
+  cat(nam, "\n")
+
   width <- 7
   height <- 7
 
-  gif_ <- sets[[i]](x, y, by = "id")
-  first_ <- sets[[i]](x, y, by = "id", export = "first")
-  last_ <- sets[[i]](x, y, by = "id", export = "last")
+  gif_ <- sets[[i]](x, y)
+  first_ <- sets[[i]](x, y, export = "first")
+  last_ <- sets[[i]](x, y, export = "last")
 
   save_animation(gif_, here("images", paste0(nam, ".gif")))
   ggsave(here("images", "static", "png", paste0(nam, "-first.png")), first_,
