608f0020a064a9415007b3598b44e63fede64219
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-09-01 13:33:25 -0400

    Import spread and gather from tidyr

--- R/zzzz-package.R
+++ R/zzzz-package.R
@@ -1,5 +1,6 @@
 #' @importFrom dplyr left_join right_join full_join inner_join semi_join anti_join
 #' @importFrom dplyr mutate select filter arrange bind_rows bind_cols group_by pull slice data_frame row_number
+#' @importFrom tidyr gather spread
 #' @keywords internal
 "_PACKAGE"
 
