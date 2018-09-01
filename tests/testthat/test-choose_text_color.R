6e19e7f83f837c2d5deb95980324c5365e6eb3ea
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-09-01 12:58:16 -0400

    Fix function name in choose_text_color test

--- tests/testthat/test-choose_text_color.R
+++ tests/testthat/test-choose_text_color.R
@@ -2,5 +2,5 @@ context("test-set_text_color")
 
 test_that("correct color selection", {
   colors <- c("#FFFFFF", scales::brewer_pal("seq", "Set1")(4), "#000000")
-  expect_equal(set_text_color(colors), c("#000000", rep("#FFFFFF", 5)))
+  expect_equal(choose_text_color(colors), c("#000000", rep("#FFFFFF", 5)))
 })
