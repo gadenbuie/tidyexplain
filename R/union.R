4fbb0871b3731c5345fc3220a765349f242be3f5
Garrick Aden-Buie <garrick@adenbuie.com>
2022-01-09 07:55:56 -0500

    Fix union animations
    Fixes #39

--- R/union.R
+++ R/union.R
@@ -1,15 +1,17 @@
 source(here::here("R/00_base_set.R"))
 
 # ---- union(x, y) ----
-uxy <- bind_rows(
-  initial_set_dfs,
-  union(x, y) %>% proc_data_set("xy") %>% mutate(frame = 2, .x = .x + 1.5),
-  intersect(x, y) %>% proc_data_set("xy") %>% mutate(frame = 2, .y = -4, .x = .x + 1.5)
-) %>%
+uxy <-
+  bind_rows(
+    initial_set_dfs,
+    union(x, y) %>% proc_data_set("xy") %>% mutate(frame = 2, .x = .x + 1.5),
+    intersect(x, y) %>% proc_data_set("xy") %>% mutate(frame = 2, .y = -1, .x = .x + 1.5)
+  ) %>%
   plot_data_set("union(x, y)", ylims = ylim(-4.5, -0.5)) %>%
-  animate_plot()
+  animate_plot(wrap = FALSE) +
+  ease_aes(y = "exponential-out")
 
-uxy <- animate(uxy)
+uxy <- animate(uxy, rewind = TRUE)
 
 anim_save(here::here("images", "union.gif"), uxy)
 
@@ -22,15 +24,17 @@ save_static_plot(uxy_g, "union")
 
 
 # ---- union(y, x) ----
-uyx <- bind_rows(
-  initial_set_dfs,
-  union(y, x) %>% proc_data_set("xy") %>% mutate(frame = 2, .x = .x + 1.5),
-  intersect(y, x) %>% proc_data_set("xy") %>% mutate(frame = 2, .y = -4, .x = .x + 1.5)
-) %>%
-  plot_data_set("union(y, x)", ylims = ylim(-4.5, -0.5)) %>%
-  animate_plot()
+uyx <-
+  bind_rows(
+    initial_set_dfs,
+    union(y, x) %>% proc_data_set("xy") %>% mutate(frame = 2, .x = .x + 1.5),
+    intersect(y, x) %>% proc_data_set("xy") %>% mutate(frame = 2, .y = -1, .x = .x + 1.5)
+  ) %>%
+  plot_data_set(., "union(y, x)", ylims = ylim(-4.5, -0.5)) %>%
+  animate_plot(wrap = FALSE) +
+  ease_aes(y = "exponential-out")
 
-uyx <- animate(uyx)
+uyx <- animate(uyx, rewind = TRUE)
 
 anim_save(here::here("images", "union-rev.gif"), uyx)
 
